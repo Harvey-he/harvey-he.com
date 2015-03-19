dcb4920f23b0382074d536a0a15d37320ee4b50c
万重 <wanli.hwl@alibaba-inc.com>
2015-03-19 23:57:54 +0800

    update layout

--- controllers/admin.js
+++ controllers/admin.js
@@ -5,22 +5,16 @@ var Topic = require('../proxy').Topic;
 
 exports.new = function (req, res) {
     var title = '标题';
-    var content = '内容内容内容内容内容内容内容内容内容内容内容内容' +
-        '内容内容内容内容内容内容内容内容内容内容内容内容内容内容' +
-        '内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容' +
-        '内容内容内容内容内容内容内容内容内容内容内容内容内容内容' +
-        '内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容' +
-        '内容内容内容内容内容内容内容内容内容内容内容' +
-        '内容内容内容内容内容内容内容内容内容内容内容内容' +
-        '内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容';
+    var content = '内容内容';
 
-    Topic.newAndSave(title, content, function (err, topic) {
-        if (err) {
-            return next(err);
-        }
-
-        res.render('admin', {'notic': '成功发表文章','topicId': topic._id});
-
-    });
+    //Topic.newAndSave(title, content, function (err, topic) {
+    //    if (err) {
+    //        return next(err);
+    //    }
+    //
+    //    res.render('admin', {'notic': '成功发表文章','topicId': topic._id});
+    //
+    //});
+    res.render('admin');
 
 }
