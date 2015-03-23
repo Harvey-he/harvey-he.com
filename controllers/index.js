f24a0a474c0f42fd2bdf816befee47f8a88bdbb5
万重 <wanli.hwl@alibaba-inc.com>
2015-03-23 23:19:31 +0800

    add moment node modules to format date

--- controllers/index.js
+++ controllers/index.js
@@ -2,13 +2,26 @@
  * Created by harvey on 3/5/15.
  */
 var Topic = require('../proxy').Topic;
+var moment = require('moment');
 
 exports.index = function (req, res) {
     Topic.getAllTopic(function (err, topics) {
+        var topicArray = [];
         if (err) {
             return next(err);
         }
-        res.render('index',{'topics':topics});
+        topics.forEach(function(topic){
+            var obj = {};
+            obj._id = topic._id;
+            obj.title = topic.title;
+            obj.content = topic.content;
+            var date = moment(topic.create_at, null, 'zh-cn');
+            var fdate = date.fromNow();
+            obj.create_at = fdate;
+            topicArray.push(obj);
+
+        })
+        res.render('index',{'topics':topicArray});
     });
 
 }
